306a93eee47cdf1e46274fb35cf2d1485640ae51
Micael Santiago <santiagoo.micael04@gmail.com>
2023-01-19 21:47:33 -0300

    Dia 4 - Profissionalizando o projeto

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const form = document.querySelector('#form-habits');
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector('header button');
+const data = JSON.parse(localStorage.getItem("db_habits")) || {};
 
-const data = {
-	run: ['01-01', '01-02', '01-06', '01-02', '01-10', '02-02', '02-05'],
-	takePills: ['01-03', '01-01', '01-02', '01-06', '02-02', '03-01', '04-02', '05-06'],
-	journal: ['01-02', '07-01', '08-02', '09-06']
+function add() {
+	const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
+	const dayExists = nlwSetup.dayExists(today);
+
+	if (dayExists) {
+		alert('Dia já incluso!');
+		return;
+	}
+
+	nlwSetup.addDay(today);
 };
 
+function save() {
+	localStorage.setItem("db_habits", JSON.stringify(nlwSetup.data));
+	localStorage.getItem("db_habits");
+	
+};
+
+button.addEventListener('click', add);
+form.addEventListener('change', save);
+
 nlwSetup.setData(data);
-nlwSetup.load()
+nlwSetup.load();
